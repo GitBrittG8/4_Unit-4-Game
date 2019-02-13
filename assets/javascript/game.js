6ce6b6d9c70b5915c27a1dfd2002877acb373de9
Rhubarb <edlun066@d.umn.edu>
2019-02-13 15:17:43 -0600

    Last save before (this) research

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,55 @@
 // Values
-var crystOne=0;
-var crystTwo=0;
-var crystThree=0;
-var crystFour=0;
+var crystOne=Math.floor(Math.random() * 12 + 1);
+var crystTwo=Math.floor(Math.random() * 12 + 1);
+var crystThree=Math.floor(Math.random() * 12 + 1);
+var crystFour=Math.floor(Math.random() * 12 + 1);
 var wins=0;
 var loss=0;
-var goalPrice=0;
+var goalPrice=Math.floor(Math.random() * 10 + 19);
 var currentBalance=0;
 
-//Set all Crystals to Random Value
-crystOne=Math.floor(Math.random() * 12 + 1);
-crystTwo=Math.floor(Math.random() * 12 + 1);
-crystThree=Math.floor(Math.random() * 12 + 1);
-crystFour=Math.floor(Math.random() * 12 + 1);
 
-// If the first two values are the same number, then reset the second crystal to a new value
-if (crystOne===crystTwo){
-        CrystTwo=Math.floor(Math.random() * 12 + 1);  
-        }
-// If the third crystal is the same value as the first or second crystal,  reset it's value
-if (crystThree===crystOne||crystTwo){
-        crystThree=Math.floor(Math.random() * 12 + 1);  
-        }
-// Reset fourth Crystal if it's value matches any previous
-if (crystFour===crystOne||crystTwo||crystThree){
-        crystFour=Math.floor(Math.random() * 12 + 1); 
+if (crystOne===crystTwo){                               
+        CrystTwo=Math.floor(Math.random() * 12 + 1);  // If first two values are equal, reset 2nd crystal
 }
-
+if (crystThree===crystOne||crystTwo){
+        crystThree=Math.floor(Math.random() * 12 + 1);  // If third crystal equals 1st or 2nd crystal,  reset it
+}
+if (crystFour===crystOne||crystTwo||crystThree){
+        crystFour=Math.floor(Math.random() * 12 + 1);  // Reset fourth Crystal if it matches any previous
+}
 
 console.log(crystOne + " is the Green Value");
 console.log(crystTwo + " is the Blue Value");
 console.log(crystThree + " is the Red Value");
 console.log(crystFour + " is the Pink Value");
-
-goalPrice=Math.floor(Math.random() * 10 + 19);              // Set goal price
-currentBalance=0;                                           // Reset 
-
 console.log(goalPrice + " is the Goal Price");
 console.log(currentBalance + " is the current balance");
 console.log("Hurray!");
 
-$("#goal-price").text(goalPrice)                              //Price in html
+var crystalValue = 5;
+                                        
+$(".crystal-image").on("click", function() {             // When crystal is clicked...  
 
-var counter = 0;
-$(".crystal-image").on("click", function() {
-        counter += 10;
-        alert("Counter is: " + counter);
-        console.log(counter + " is the counter value")
+                                                        // Find the id of the crystal
+                                                        // Grab the numerical value from that spec id
+
+        //var crystalValue = ($(this).class("crystal-image")); // Grabs correct crystal value
+        //crystalValue = parseInt(crystalValue);               // Finds it's Numerical value
+
+        currentBalance += crystalValue;                        // Adds value(s) to current balance
+
+        if (currentBalance === goalPrice) {                     // Display win/loss
+                alert("You win!");
+                ++ wins;
+        }
+        if (currentBalance > goalPrice) {
+                alert("You lose!");
+                ++ loss;
+        }
+
+        $("#goal-price").text(goalPrice);                         // Place stats in html
+        $("#current-balance").text(currentBalance);
+        $("#stats").text("Wins: "+wins+" Losses: "+loss);
 });
+
