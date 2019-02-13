df198b5933747d8e2e1a2547f95fb6539bcd5e5e
Rhubarb <edlun066@d.umn.edu>
2019-02-13 16:18:35 -0600

    Need to fix source in Basic and selectors in Complex

---
+++ Basic Version/assets/javascript/game.js
@@ -0,0 +1,33 @@
+var goalPrice = 30;
+      
+$("#goal-price").text(goalPrice);
+
+var increment = 0;
+var numberArray = [10, 5, 3, 7];
+
+for (var i = 0; i < numberArray.length; i++) {
+
+        var crystalImg = $("<img>");
+
+        crystalImg.addClass("crystal-image");
+        crystalImg.attr('src', '../assets/images/Geschliffener_blauer_Saphir.jpg');
+        crystalImg.attr("data-crystalvalue", numberArray[i]);  // Add data attribute, set it equal to the array value.
+        $("#crystals").append(crystalImg); // image added to the page
+}
+
+$(".crystal-image").on("click", function() {
+
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+       
+        increment += crystalValue;
+         alert("New score: " + increment);
+
+        if (increment === goalPrice) {
+        alert("You win!");
+        }
+
+        else if (increment >= goalPrice) {
+        alert("You lose!!");
+        }
+});
