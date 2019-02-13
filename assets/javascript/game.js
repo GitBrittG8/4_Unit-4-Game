ef3d5008e554dc4ee8557dfc97358ecef3ab4b98
Rhubarb <edlun066@d.umn.edu>
2019-02-12 21:28:17 -0600

    Click Crystal Basic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,87 +8,34 @@ var loss=0;
 var goalPrice=0;
 var currentBalance=0;
 
-    // Reset
-    function reset(){
-        
-        //Set all Crystals to Random Value
-        crystOne=Math.floor(Math.random() * 12 + 1);
-        crystTwo=Math.floor(Math.random() * 12 + 1);
-        crystThree=Math.floor(Math.random() * 12 + 1);
-        crystFour=Math.floor(Math.random() * 12 + 1);
-        
-        // If the first two values are the same number, then reset the second crystal to a new value
-        if (crystOne===crystTwo){
-                CrystTwo=Math.floor(Math.random() * 12 + 1);  
-                }
-        // If the third crystal is the same value as the first or second crystal,  reset it's value
-        if (crystThree===crystOne||crystThree===crystTwo){
-                crystThree=Math.floor(Math.random() * 12 + 1);  
-                }
-        // Reset fourth Crystal if it's value matches any previous
-        if (crystFour===crystOne||crystFour===crystTwo||crystFour===crystThree){
-                crystFour=Math.floor(Math.random() * 12 + 1); 
+//Set all Crystals to Random Value
+crystOne=Math.floor(Math.random() * 12 + 1);
+crystTwo=Math.floor(Math.random() * 12 + 1);
+crystThree=Math.floor(Math.random() * 12 + 1);
+crystFour=Math.floor(Math.random() * 12 + 1);
+
+// If the first two values are the same number, then reset the second crystal to a new value
+if (crystOne===crystTwo){
+        CrystTwo=Math.floor(Math.random() * 12 + 1);  
         }
-        
+// If the third crystal is the same value as the first or second crystal,  reset it's value
+if (crystThree===crystOne||crystTwo){
+        crystThree=Math.floor(Math.random() * 12 + 1);  
+        }
+// Reset fourth Crystal if it's value matches any previous
+if (crystFour===crystOne||crystTwo||crystThree){
+        crystFour=Math.floor(Math.random() * 12 + 1); 
+}
 
-        console.log(crystOne);
-        console.log(crystTwo);
-        console.log(crystThree);
-        console.log(crystFour);
 
-        goalPrice=Math.floor(Math.random() * 101 + 19);
-        currentBalance=0;
-        $("#targetNumber").text(goalPrice);
-        $("#scoreBox").text(currentBalance);
-        $("#winLoss").text("Wins : "+wins+" || Losses: "+loss);
-    }
-    reset();
+console.log(crystOne + " is the Green Value");
+console.log(crystTwo + " is the Blue Value");
+console.log(crystThree + " is the Red Value");
+console.log(crystFour + " is the Pink Value");
 
-    // Functions for Button Clicks still need to be fixed 
-    /*
-    $("#btnOne").click(function(){
-        $(this).finish();
-        currentBalance+=crystOne;
-        $("#scoreBox").text(currentBalance);
-       
-    });
+goalPrice=Math.floor(Math.random() * 10 + 19);              // Set goal price
+currentBalance=0;                                           // Reset 
 
-    $("#btnTwo").click(function(){
-        $(this).finish();
-        currentBalance+=crystTwo;
-        $("#scoreBox").text(currentBalance);
-       
-    });
-
-    $("#btnThree").click(function(){
-        $(this).finish();
-        currentBalance+=crystThree;
-        $("#scoreBox").text(currentBalance);
-       
-    });
-
-    $("#btnFour").click(function(){
-        $(this).finish();
-        currentBalance+=crystFour;
-        $("#scoreBox").text(currentBalance);      
-       
-    });
-    */
-    // win/loss detector tied to the crystal button class
-    $(".crystalBtn").click(function(){
-        if (currentBalance===goalPrice){
-            wins ++;
-            alert("You win!");
-            reset();
-
-        }  else if (currentBalance>goalPrice){
-            loss++;
-            alert("You Lose...");
-            reset();
-
-        }  
-    });
-    
-    console.log(goalPrice)
-    console.log(currentBalance)
-    
+console.log(goalPrice + " is the Goal Price");
+console.log(currentBalance + " is the current balance");
+console.log("Hurray!");
